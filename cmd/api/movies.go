1d7ad058151a5bcb260195e9e7825b64cd12a63a
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-21 08:43:14 +0530

    handlers defined on movie type

---
+++ cmd/api/movies.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"FernArchive/internal/data"
+)
+
+func (bknd *backend) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintln(w, "create a new movie")
+}
+
+func (bknd *backend) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := bknd.readIdParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	movie := data.Movie{
+		Id:        id,
+		CreatedAt: time.Now(),
+		Title:     "Infinity War",
+		Runtime:   102,
+		Genres:    []string{"action", "sci-fi", "war"},
+		Version:   1,
+	}
+	err = bknd.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		bknd.logger.Error(err.Error())
+		http.Error(w, "could not process your request", http.StatusInternalServerError)
+	}
+}
+
+func (bknd *backend) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintln(w, "update a movie")
+}
