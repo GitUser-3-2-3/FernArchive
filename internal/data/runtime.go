f653a5954000f4b79beb8661f9e1319d504f7482
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-22 21:28:03 +0530

    added method to unmarshal runtime value

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,14 +1,37 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
+//goland:noinspection GoMixedReceiverTypes
 func (rtm Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", rtm)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+//goland:noinspection GoMixedReceiverTypes
+func (rtm *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	val, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*rtm = Runtime(val)
+	return nil
 }
