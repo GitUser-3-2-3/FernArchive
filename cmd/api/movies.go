078d49bcb8ea16f14ca32d3a401b8d2e53342497
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-22 17:23:47 +0530

    uses the new helper method readJSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -16,7 +15,7 @@ func (bknd *backend) createMovieHandler(w http.ResponseWriter, r *http.Request) 
 		Runtime int32    `json:"runtime"`
 		Genre   []string `json:"genres"`
 	}
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := bknd.readJSON(w, r, &input)
 	if err != nil {
 		bknd.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
 		return
