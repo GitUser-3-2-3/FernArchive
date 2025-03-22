ba2c742a5be57e86665f68b7ab7cc6f669cd8170
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-22 13:50:05 +0530

    relevant error methods for consistency throughout backend

---
+++ cmd/api/errors.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (bknd *backend) logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+	bknd.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+func (bknd *backend) errorResponseJSON(w http.ResponseWriter, r *http.Request, status int, msg any) {
+	env := envelope{"error": msg}
+	err := bknd.writeJSON(w, status, env, nil)
+	if err != nil {
+		bknd.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (bknd *backend) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("%s method not supported for this request", r.Method)
+	bknd.errorResponseJSON(w, r, http.StatusMethodNotAllowed, msg)
+}
+
+func (bknd *backend) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	msg := "requested resource not found"
+	bknd.errorResponseJSON(w, r, http.StatusNotFound, msg)
+}
+
+func (bknd *backend) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	bknd.logError(r, err)
+	msg := "server encountered a problem and could not process your request"
+	bknd.errorResponseJSON(w, r, http.StatusInternalServerError, msg)
+}
