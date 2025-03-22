6ef16eb957f40f8909720c38b2229d96c2ca2186
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-22 16:57:44 +0530

    basic impl of create movie handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -8,8 +9,19 @@ import (
 	"FernArchive/internal/data"
 )
 
-func (bknd *backend) createMovieHandler(w http.ResponseWriter, _ *http.Request) {
-	_, _ = fmt.Fprintln(w, "create a new movie")
+func (bknd *backend) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int      `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genre   []string `json:"genres"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		bknd.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	_, _ = fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (bknd *backend) showMovieHandler(w http.ResponseWriter, r *http.Request) {
