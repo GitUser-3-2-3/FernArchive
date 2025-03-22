4edb394bbbf812034277348ec3f19e3d3e605137
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-22 18:24:17 +0530

    max limit added for json request, and separated error check logic

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -39,31 +40,51 @@ func (bknd *backend) writeJSON(w http.ResponseWriter, status int, data envelope,
 	return nil
 }
 
-func (bknd *backend) readJSON(_ http.ResponseWriter, r *http.Request, dst any) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
-	if err != nil {
-		var unmarshalTypeError *json.UnmarshalTypeError
-		var syntaxError *json.SyntaxError
-		var invalidUnmarshalError *json.InvalidUnmarshalError
+func (bknd *backend) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
-		switch {
-		case errors.As(err, &syntaxError):
-			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			return fmt.Errorf("body contains badly-formed JSON")
-		case errors.As(err, &invalidUnmarshalError):
-			panic(err)
-		case errors.Is(err, io.EOF):
-			return fmt.Errorf("body must not be empty")
-		case errors.As(err, &unmarshalTypeError):
-			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf(
-					"body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
-			}
-			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
-		default:
-			return err
-		}
+	dcdr := json.NewDecoder(r.Body)
+	dcdr.DisallowUnknownFields()
+
+	err := dcdr.Decode(dst)
+	if err != nil {
+		return bknd.decodeJSONError(err)
+	}
+	err = dcdr.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must contain exactly one JSON object")
 	}
 	return nil
+}
+
+func (bknd *backend) decodeJSONError(err error) error {
+	var unmarshalTypeError *json.UnmarshalTypeError
+	var syntaxError *json.SyntaxError
+	var invalidUnmarshalError *json.InvalidUnmarshalError
+	var maxBytesError *http.MaxBytesError
+
+	switch {
+	case errors.As(err, &syntaxError):
+		return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+	case errors.Is(err, io.ErrUnexpectedEOF):
+		return fmt.Errorf("body contains badly-formed JSON")
+	case errors.Is(err, io.EOF):
+		return fmt.Errorf("body must not be empty")
+	case errors.As(err, &unmarshalTypeError):
+		if unmarshalTypeError.Field != "" {
+			return fmt.Errorf(
+				"body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+		}
+		return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+	case strings.HasPrefix(err.Error(), "json: unknown field "):
+		fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+		return fmt.Errorf("body contains unknown JSON field for key %s", fieldName)
+	case errors.As(err, &maxBytesError):
+		return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+	case errors.As(err, &invalidUnmarshalError):
+		panic(err)
+	default:
+		return err
+	}
 }
