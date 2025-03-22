8855f4554d9b37137ba112a11e99fe816f4513bf
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-22 13:59:28 +0530

    redefined error methods on httprouter to use custom error handling

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,6 +8,10 @@ import (
 
 func (bknd *backend) routes() http.Handler {
 	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(bknd.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(bknd.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", bknd.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", bknd.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", bknd.showMovieHandler)
