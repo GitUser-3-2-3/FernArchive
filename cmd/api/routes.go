963c58bd8b7d9a422212446fe331f055764f6de3
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-23 10:25:30 +0530

    cleanup

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,11 +9,12 @@ import (
 func (bknd *backend) routes() http.Handler {
 	router := httprouter.New()
 
-	router.NotFound = http.HandlerFunc(bknd.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(bknd.methodNotAllowedResponse)
+	router.NotFound = http.HandlerFunc(bknd.notFoundResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", bknd.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", bknd.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", bknd.showMovieHandler)
+
 	return bknd.recoverPanic(router)
 }
