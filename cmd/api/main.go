0a577812600f7f7d7e00f89c8e97d4eaa2df5055
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-23 16:12:04 +0530

    database related changes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -9,11 +11,16 @@ import (
 	"time"
 )
 
+import _ "github.com/lib/pq"
+
 const version = "1.0.0"
 
 type config struct {
-	port int
 	env  string
+	port int
+	db   struct {
+		dsn string
+	}
 }
 
 type backend struct {
@@ -26,9 +33,22 @@ func main() {
 
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev, staging, prod)")
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.db.dsn, "db-dsn",
+		"postgres://fern_archive:Qwerty1,0*@localhost/fern_archive?sslmode=disable", "PostgresSQL DSN")
 	flag.Parse()
-
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			logger.Error(err.Error())
+		}
+	}(db)
+	logger.Info("Database connection established")
 	bknd := &backend{
 		logger: logger,
 		config: cfg,
@@ -42,9 +62,22 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 	logger.Info("API server started", "addrs", srvr.Addr, "env", cfg.env)
-	err := srvr.ListenAndServe()
+	err = srvr.ListenAndServe()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
