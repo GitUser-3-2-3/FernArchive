0230a29a72dc1450bb1ffc21646862de0217ace9
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-25 15:57:51 +0530

    update method checks version number to fortify against race conditions

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -56,10 +56,20 @@ func (mdl *MovieModel) Get(id int64) (*Movie, error) {
 
 func (mdl *MovieModel) Update(movie *Movie) error {
 	query := `UPDATE movies SET title=$1, year=$2, runtime=$3, genres=$4, version=version+1
-                WHERE id = $5 RETURNING version`
+                WHERE id = $5 AND version=$6 RETURNING version`
 
-	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.Id}
-	return mdl.DB.QueryRow(query, args...).Scan(&movie.Version)
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.Id,
+		movie.Version}
+	err := mdl.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (mdl *MovieModel) Delete(id int64) error {
