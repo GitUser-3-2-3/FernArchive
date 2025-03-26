ac4e169d89b63d55d29df9b647d77e3e05fec44f
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-26 15:55:12 +0530

    struct and func defined to return metadata along with response

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -46,3 +46,24 @@ func ValidateFilters(vldtr *validator.Validator, fltr Filters) {
 
 	vldtr.Check(validator.PermittedValue(fltr.Sort, fltr.SortParams...), "sort", "invalid sort value")
 }
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func CalculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
