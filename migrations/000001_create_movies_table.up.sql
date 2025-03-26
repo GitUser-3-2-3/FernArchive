6e3397d12e93bab484f087f16c5098a82c1ca0b1
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-26 11:42:59 +0530

    migration files

---
+++ migrations/000001_create_movies_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS movies
+(
+    id         bigserial primary key,
+    created_at timestamp(0) with time zone not null default now(),
+    title      text                        not null,
+    year       integer                     not null,
+    runtime    integer                     not null,
+    genres     text[]                      not null,
+    version    integer                     not null default 1
+);
